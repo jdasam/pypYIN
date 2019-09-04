316bf5e1a60f6b9f39e1967a91cc63834f75e86e
Stefan Sullivan <stefan.sullivan@smule.com>
2019-09-04 16:51:34 -0700

    create a setup.py that allows installing from pip

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    with open('README.md') as fh:
+        readme_contents = fh.read()
+
+    setup(name='pypyin',
+          version='1.0',
+          author='Rong GONG',
+          url='https://github.com/stefansjs/pypYIN',
+
+          description='pYIN algorithm implemented in python. https://code.soundsoftware.ac.uk/projects/pyin',
+          long_description=readme_contents,
+          long_description_content_type='text/markdown',
+
+          # Get all the packages from the src directory and remap them to root packages
+          packages=['pypyin'],
+          package_dir={'': 'src'},
+          scripts=['demo.py'],
+
+          install_requires=['numpy',
+                            'scipy',
+                            'essentia'],
+          )
